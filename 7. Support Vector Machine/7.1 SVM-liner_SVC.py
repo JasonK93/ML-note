dec8076f1256791a0094545f7bfb944c0e83db94
Vam <xueli81993@gmail.com>
2017-07-06 19:40:51 +0800

    SVM-liner_SVC

---
+++ 7. Support Vector Machine/7.1 SVM-liner_SVC.py
@@ -0,0 +1,165 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model,cross_validation,svm
+
+def load_data_classfication():
+    '''
+    load iris data set
+    :return: train_data,test_data, train_target, test_target
+    '''
+    iris=datasets.load_iris()
+    X_train=iris.data
+    y_train=iris.target
+    return cross_validation.train_test_split(X_train, y_train,test_size=0.25,
+		random_state=0,stratify=y_train)
+
+def test_SVC_linear(*data):
+    '''
+    test method of SVC
+    :param data:  train_data,test_data, train_target, test_target
+    :return: None
+    '''
+    X_train,X_test,y_train,y_test=data
+    cls=svm.SVC(kernel='linear')
+    cls.fit(X_train,y_train)
+    print('Coefficients:{0}, intercept {1}'.format(cls.coef_,cls.intercept_))
+    print('Score: {0}' .format(cls.score(X_test, y_test)))
+def test_SVC_poly(*data):
+    '''
+    test the performance with different degree, gamma, codf0
+    :param data:  train_data,test_data, train_target, test_target
+    :return: None
+    '''
+    X_train,X_test,y_train,y_test=data
+    fig=plt.figure()
+    ### test degree ####
+    degrees=range(1,20)
+    train_scores=[]
+    test_scores=[]
+    for degree in degrees:
+        cls=svm.SVC(kernel='poly',degree=degree)
+        cls.fit(X_train,y_train)
+        train_scores.append(cls.score(X_train,y_train))
+        test_scores.append(cls.score(X_test, y_test))
+    ax=fig.add_subplot(1,3,1)
+    ax.plot(degrees,train_scores,label="Training score ",marker='+' )
+    ax.plot(degrees,test_scores,label= " Testing  score ",marker='o' )
+    ax.set_title( "SVC_poly_degree ")
+    ax.set_xlabel("p")
+    ax.set_ylabel("score")
+    ax.set_ylim(0,1.05)
+    ax.legend(loc="best",framealpha=0.5)
+
+    ### test gamma ， degree fixed with 3####
+    gammas=range(1,20)
+    train_scores=[]
+    test_scores=[]
+    for gamma in gammas:
+        cls=svm.SVC(kernel='poly',gamma=gamma,degree=3)
+        cls.fit(X_train,y_train)
+        train_scores.append(cls.score(X_train,y_train))
+        test_scores.append(cls.score(X_test, y_test))
+    ax=fig.add_subplot(1,3,2)
+    ax.plot(gammas,train_scores,label="Training score ",marker='+' )
+    ax.plot(gammas,test_scores,label= " Testing  score ",marker='o' )
+    ax.set_title( "SVC_poly_gamma ")
+    ax.set_xlabel(r"$\gamma$")
+    ax.set_ylabel("score")
+    ax.set_ylim(0,1.05)
+    ax.legend(loc="best",framealpha=0.5)
+    ### test r ， gamma fixed with 10 ， degree fixed with 3######
+    rs=range(0,20)
+    train_scores=[]
+    test_scores=[]
+    for r in rs:
+        cls=svm.SVC(kernel='poly',gamma=10,degree=3,coef0=r)
+        cls.fit(X_train,y_train)
+        train_scores.append(cls.score(X_train,y_train))
+        test_scores.append(cls.score(X_test, y_test))
+    ax=fig.add_subplot(1,3,3)
+    ax.plot(rs,train_scores,label="Training score ",marker='+' )
+    ax.plot(rs,test_scores,label= " Testing  score ",marker='o' )
+    ax.set_title( "SVC_poly_r ")
+    ax.set_xlabel(r"r")
+    ax.set_ylabel("score")
+    ax.set_ylim(0,1.05)
+    ax.legend(loc="best",framealpha=0.5)
+    plt.show()
+def test_SVC_rbf(*data):
+    '''
+    test SVC with Gaussian kernel and different gamma
+    :param data:  train_data,test_data, train_target, test_target
+    :return: None
+    '''
+    X_train,X_test,y_train,y_test=data
+    gammas=range(1,20)
+    train_scores=[]
+    test_scores=[]
+    for gamma in gammas:
+        cls=svm.SVC(kernel='rbf',gamma=gamma)
+        cls.fit(X_train,y_train)
+        train_scores.append(cls.score(X_train,y_train))
+        test_scores.append(cls.score(X_test, y_test))
+    fig=plt.figure()
+    ax=fig.add_subplot(1,1,1)
+    ax.plot(gammas,train_scores,label="Training score ",marker='+' )
+    ax.plot(gammas,test_scores,label= " Testing  score ",marker='o' )
+    ax.set_title( "SVC_rbf")
+    ax.set_xlabel(r"$\gamma$")
+    ax.set_ylabel("score")
+    ax.set_ylim(0,1.05)
+    ax.legend(loc="best",framealpha=0.5)
+    plt.show()
+def test_SVC_sigmoid(*data):
+    '''
+    test SVC with sigmoid kernel with different gamma and coef0
+    :param data:  train_data,test_data, train_target, test_target
+    :return: None
+    '''
+    X_train,X_test,y_train,y_test=data
+    fig=plt.figure()
+
+    ### test gamma ，fixed coef0 with 0 ####
+    gammas=np.logspace(-2,1)
+    train_scores=[]
+    test_scores=[]
+
+    for gamma in gammas:
+        cls=svm.SVC(kernel='sigmoid',gamma=gamma,coef0=0)
+        cls.fit(X_train,y_train)
+        train_scores.append(cls.score(X_train,y_train))
+        test_scores.append(cls.score(X_test, y_test))
+    ax=fig.add_subplot(1,2,1)
+    ax.plot(gammas,train_scores,label="Training score ",marker='+' )
+    ax.plot(gammas,test_scores,label= " Testing  score ",marker='o' )
+    ax.set_title( "SVC_sigmoid_gamma ")
+    ax.set_xscale("log")
+    ax.set_xlabel(r"$\gamma$")
+    ax.set_ylabel("score")
+    ax.set_ylim(0,1.05)
+    ax.legend(loc="best",framealpha=0.5)
+    ### test r，fixed gamma with 0.01 ######
+    rs=np.linspace(0,5)
+    train_scores=[]
+    test_scores=[]
+
+    for r in rs:
+        cls=svm.SVC(kernel='sigmoid',coef0=r,gamma=0.01)
+        cls.fit(X_train,y_train)
+        train_scores.append(cls.score(X_train,y_train))
+        test_scores.append(cls.score(X_test, y_test))
+    ax=fig.add_subplot(1,2,2)
+    ax.plot(rs,train_scores,label="Training score ",marker='+' )
+    ax.plot(rs,test_scores,label= " Testing  score ",marker='o' )
+    ax.set_title( "SVC_sigmoid_r ")
+    ax.set_xlabel(r"r")
+    ax.set_ylabel("score")
+    ax.set_ylim(0,1.05)
+    ax.legend(loc="best",framealpha=0.5)
+    plt.show()
+if __name__=="__main__":
+    X_train,X_test,y_train,y_test=load_data_classfication()
+    test_SVC_linear(X_train,X_test,y_train,y_test)
+    test_SVC_poly(X_train,X_test,y_train,y_test)
+    test_SVC_rbf(X_train,X_test,y_train,y_test)
+    test_SVC_sigmoid(X_train,X_test,y_train,y_test) 
