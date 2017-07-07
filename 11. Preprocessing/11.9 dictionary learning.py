38a887ca750f48fce919fa6782d68103726b3f08
Vam <xueli81993@gmail.com>
2017-07-07 23:23:17 +0800

    dictionary learning

---
+++ 11. Preprocessing/11.9 dictionary learning.py
@@ -0,0 +1,19 @@
+from sklearn.decomposition import DictionaryLearning
+
+def test_DictionaryLearning():
+    '''
+    test the DL method
+    :return: None
+    '''
+    X=[[1,2,3,4,5],
+       [6,7,8,9,10],
+       [10,9,8,7,6,],
+       [5,4,3,2,1] ]
+    print("before transform:",X)
+    dct=DictionaryLearning(n_components=3)
+    dct.fit(X)
+    print("components is :",dct.components_)
+    print("after transform:",dct.transform(X))
+
+if __name__=='__main__':
+    test_DictionaryLearning() 
